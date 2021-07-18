24307053922e23e526fa5224bbb5466abdbdcc79
X-USERBOT <87603169+X-USERBOT@users.noreply.github.com>
2021-07-18 19:12:38 +0530

    Add files via upload

--- xbot/config/vars.py
+++ xbot/config/vars.py
@@ -1,1 +1,13 @@
+# Configs imports from here
 
+import os
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    from hell_config import Config
+else:
+    if os.path.exists("config.py"):
+        from config import Development as Config
+
+# hellbot
