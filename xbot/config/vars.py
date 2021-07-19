a8466ce825e3d94ee7bd12e2b8c5f793a6f8e32d
X-USERBOT <87603169+X-USERBOT@users.noreply.github.com>
2021-07-19 12:06:52 +0530

    Update vars.py

--- xbot/config/vars.py
+++ xbot/config/vars.py
@@ -5,7 +5,7 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from hell_config import Config
+    from xbot_config import Config
 else:
     if os.path.exists("config.py"):
         from config import Development as Config
