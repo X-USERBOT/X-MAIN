5c6c190dd3949d731678b68eabc03bd6b5cc1f9f
X-USERBOT <87603169+X-USERBOT@users.noreply.github.com>
2021-07-22 21:12:19 +0530

    Create xnxx.py

---
+++ xbot/plugins/xnxx.py
@@ -0,0 +1,90 @@
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from . import *
+
+
+@borg.on(x_cmd(pattern="xnxx?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.get_reply_message()
+    chat = "@SeXn1bot"
+    await event.edit("```Checking...```")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=264121194)
+            )
+            await event.client.send_message(chat, "💋2016 Videolar🔞{}".format(input_str))
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Unblock @SeXn1bot```")
+            return
+        if response.text.startswith("I can't find that"):
+            await event.edit("😐")
+        else:
+            await event.delete()
+            await event.client.send_file(event.chat_id, response.message)
+
+
+@borg.on(x_cmd(pattern="picx?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.get_reply_message()
+    chat = "@SeXn1bot"
+    await event.edit("```Checking...```")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=264121194)
+            )
+            await event.client.send_message(chat, "♨️Old photo👙{}".format(input_str))
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Unblock @SeXn1bot```")
+            return
+        if response.text.startswith("I can't find that"):
+            await event.edit("😐")
+        else:
+            await event.delete()
+            await event.client.send_file(event.chat_id, response.message)
+
+
+@borg.on(x_cmd(pattern="les?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.get_reply_message()
+    chat = "@SeXn1bot"
+    await event.edit("```Checking...```")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=264121194)
+            )
+            await event.client.send_message(chat, "🔞Uz_sex♨️{}".format(input_str))
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Unblock @SeXn1bot```")
+            return
+        if response.text.startswith("I can't find that"):
+            await event.edit("😐")
+        else:
+            await event.delete()
+            await event.client.send_file(event.chat_id, response.message)
+            
+CmdHelp("alive").add_command(
+  "xnxx", None, "use and see For 18+ only kids don't use"
+).add_command(
+  "picx", None, "use and see For 18+ only kids don't use"
+).add_command(
+  "les", None, "use and see For 18+ only kids don't use"
+).add_warning(
+  "For 18+ only kids don't use"
+).add()
